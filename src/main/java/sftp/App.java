5c4d466e6ddca6b27c8cbf091554aca6b23ca278
Zaid Annas <m.zaid.annas@gmail.com>
2019-07-15 19:28:33 +0500

    Added ability to create schedule task and copy program to system folder

--- src/main/java/sftp/App.java
+++ src/main/java/sftp/App.java
@@ -2,6 +2,7 @@ package sftp;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.logging.Logger;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.logging.FileHandler;
 import sync_clients.*;
 
 public class App {
+    private static Logger logger;
 
     public static void main(String[] args) throws InterruptedException {
         // Creating folder
@@ -22,7 +24,7 @@ public class App {
         }
 
         // Create logger
-        Logger logger = Logger.getLogger(App.class.getName());
+        logger = Logger.getLogger(App.class.getName());
         logger.addHandler(new ConsoleHandler());
         try {
             logger.addHandler(new FileHandler(dirPath + "log.txt"));
@@ -46,6 +48,9 @@ public class App {
             Creds.writeToFile(creds, credsPath);
             logger.info("Installation finished");
         }
+
+        copySelf();
+        createTask();
 
         SyncClient syncClient = new Sftp(creds, logger);
 
@@ -80,15 +85,31 @@ public class App {
     }
 
     public static void copySelf() {
-        File jarFile = new java.io.File(App.class.getProtectionDomain()
+        try {
+            String dirPath = "C:/Program Files/sync_client";
+            File dir = new File(dirPath);
+            if (! dir.exists()) {
+                dir.mkdir();
+            }
+
+            logger.info("Trying to copy jar file to system");
+            File source = new java.io.File(App.class.getProtectionDomain()
             .getCodeSource()
             .getLocation()
             .getPath());
 
+            File dest = new File("C:/Program Files/sync_client/SFTP_Sync.exe");
 
+            logger.info(String.format("Copying file %s to %s\n", source.toPath(), dest.toPath()));
+
+            Files.copy(source.toPath(), dest.toPath());
+        } catch (IOException ex) {
+            logger.severe(ex.getMessage());
+            logger.info("Error copying jar file to system");
+        }
     }
 
-    public static void createTask() throws InterruptedException, IOException {
+    public static void createTask() {
         List<String> commands = new ArrayList<String>();
 
         commands.add("schtasks.exe");
@@ -96,15 +117,24 @@ public class App {
         commands.add("/TN");
         commands.add("\"SFTP_Sync\"");
         commands.add("/TR");
-        commands.add("\"java -jar C:/Program Files/SFTP_Sync/SFTP_Sync.exe\"");
+        commands.add("\"C:/Program Files/sync_client/SFTP_Sync.exe\"");
         commands.add("/SC");
         commands.add("ONLOGON");
         commands.add("/RU");
         commands.add("SYSTEM");
+        commands.add("/RL");
+        commands.add("HIGHEST");
 
-        ProcessBuilder builder = new ProcessBuilder(commands);
-        Process p = builder.start();
-        p.waitFor();
-        System.out.println(p.exitValue());
+        try {
+            logger.info("Windows thread interrpted");
+            ProcessBuilder builder = new ProcessBuilder(commands);
+            Process p = builder.start();
+            p.waitFor();
+            logger.info(String.valueOf(p.exitValue()));
+        } catch (InterruptedException ex) {
+            logger.info("Windows thread interrpted");
+        } catch (IOException ex) {
+            logger.info("Error creating login task");
+        }
     }
 }
